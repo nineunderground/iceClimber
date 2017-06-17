4090cbc1df71c35227a69a9b9ebd4a2d0d18c493
Iñaki rodriguez <nineunderground@gmail.com>
2017-06-17 16:13:58 +0300

    Draft popup created for setting tiles

--- src/components/ui/ViewHelper/Components.js
+++ src/components/ui/ViewHelper/Components.js
@@ -5,14 +5,15 @@
  * @Project: Ice Climber
  * @Filename: Components.js
  * @Last modified by:   inaki
- * @Last modified time: 15-Jun-2017
+ * @Last modified time: 17-Jun-2017
  */
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { Cell } from 'react-native-tableview-simple';
 
 const tileSiluetteImg = require('../../../res/crown.png');
 
-
+// Floor
 export class Floor extends Component {
    constructor(props) {
      super(props);
@@ -65,3 +66,138 @@ export class Floor extends Component {
        );
    }
 }
+
+// DialogCharacterRow
+export class DialogCharacterRow extends Component {
+  constructor(props) {
+   super(props);
+    this.state = {
+      imgALocation: props.imgA,
+      imgBLocation: props.imgB,
+      imgCLocation: props.imgC,
+      mainJustify: props.mainJustify,
+      imageASelected: false,
+      imageBSelected: false,
+      imageCSelected: false,
+      gameView: props.gameView,
+    };
+  }
+
+  setImageASelected(val) {
+    const newState = this.state;
+    newState.imageASelected = val;
+    // TODO Check what to set
+    // newState.isFirstFloorEnabled = val;
+    // newState.isSecondFloorEnabled = val;
+    // newState.isThirdFloorEnabled = val;
+    // newState.isFourthFloorEnabled = val;
+    // newState.isFifthFloorEnabled = val;
+    // newState.isThroneFloorEnabled = val;
+    // newState.imageBSelected = !val;
+    // newState.imageCSelected = !val;
+    this.setState(newState);
+    this.state.gameView.enableButtons(val);
+  }
+
+  setImageBSelected(val) {
+    const newState = this.state;
+    newState.imageBSelected = val;
+    // newState.imageASelected = !val;
+    // newState.imageCSelected = val;
+    this.setState(newState);
+    this.state.gameView.enableButtons(val);
+  }
+
+  setImageCSelected(val) {
+    const newState = this.state;
+    newState.imageCSelected = val;
+    // newState.imageASelected = !val;
+    // newState.imageBSelected = !val;
+    this.setState(newState);
+    this.state.gameView.enableButtons(val);
+  }
+
+// eslint-disable-line global-require
+  render() {
+    return (
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: this.state.mainJustify, alignItems: 'center' }}>
+        <TouchableOpacity onPress={() => { this.setImageASelected(!this.state.imageASelected); }}>
+          <Image
+            source={this.state.imgALocation}
+            resizeMode={'contain'}
+            style={this.state.imageASelected ? characterImgSelectedStyle : characterImgNotSelectedStyle}
+          / >
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { this.setImageBSelected(!this.state.imageBSelected); }}>
+        <Image
+          source={this.state.imgBLocation}
+          resizeMode={'contain'}
+          style={this.state.imageBSelected ? characterImgSelectedStyle : characterImgNotSelectedStyle}
+        / >
+        </TouchableOpacity>
+        {this.state.imgCLocation !== 'undefined' ? (
+          <TouchableOpacity onPress={() => { this.setImageCSelected(!this.state.imageCSelected); }}>
+          <Image
+          source={this.state.imgCLocation}
+          resizeMode={'contain'}
+          style={this.state.imageCSelected ? characterImgSelectedStyle : characterImgNotSelectedStyle}
+          / >
+          </TouchableOpacity>
+        ) : (
+          <div style={{ flex: 0 }} />
+        )}
+      </View>
+    );
+  }
+}
+
+// eslint-enable-line global-require
+//
+// PlayerCell
+export const PlayerCell = (props) => (
+  <Cell
+    {...props}
+    cellContentView={
+      <View
+        style={{ alignItems: 'center', flexDirection: 'row', flex: 1, paddingVertical: 10 }}
+      >
+        <Text
+          allowFontScaling
+          numberOfLines={1}
+          style={{ flex: 1, fontSize: 20 }}
+        >
+          {props.playerName}
+        </Text>
+
+        <Text
+          allowFontScaling
+          numberOfLines={1}
+          style={{ flex: 1, fontSize: 20 }}
+        >
+          Score {props.currentScore}
+        </Text>
+      </View>
+    }
+  />
+);
+
+const characterImgNotSelectedStyle = {
+  width: 80,
+  height: 60,
+  borderTopLeftRadius: 5,
+  borderTopRightRadius: 5,
+  opacity: 0.4,
+  margin: 10,
+  shadowOpacity: 0.9
+};
+
+const characterImgSelectedStyle = {
+  width: 80,
+  height: 60,
+  borderTopLeftRadius: 5,
+  borderTopRightRadius: 5,
+  opacity: 1.0,
+  margin: 10,
+  shadowOpacity: 0.9,
+  shadowColor: 'blue',
+};
--- src/components/ui/views/GameBoardView.js
+++ src/components/ui/views/GameBoardView.js
@@ -5,15 +5,15 @@
  * @Project: Ice Climber
  * @Filename: GameBoard.js
  * @Last modified by:   inaki
- * @Last modified time: 16-Jun-2017
+ * @Last modified time: 17-Jun-2017
  */
 import React, { Component } from 'react';
-import { View, AlertIOS, Text } from 'react-native';
+import { View, AlertIOS, Text, Image } from 'react-native';
 import Button from 'react-native-button';
 import PopupDialog, { DialogTitle } from 'react-native-popup-dialog';
-import { Floor } from './../ViewHelper/Components';
+import { Section, TableView } from 'react-native-tableview-simple';
+import { Floor, PlayerCell, DialogCharacterRow } from './../ViewHelper/Components';
 import { GameBoardStyles } from './../styles/GlobalStyles';
-import { Cell, Section, TableView } from 'react-native-tableview-simple';
 
 // Private fields
 const TOTAL_CHARACTER_TILES = 13;
@@ -22,9 +22,9 @@ const TOTAL_ROUNDS = 3;
 // Player score details
 const playerOne = {
   name: 'Mark',
-  score_1: 0,
-  score_2: 0,
-  score_3: 0,
+  score_1: 1,
+  score_2: 2,
+  score_3: 3,
 };
 const playerTwo = {
   name: 'Anna',
@@ -70,6 +70,7 @@ export default class GameBoardView extends Component {
     geTotalPlayers = props.root.state.totalPlayers;
     gePlayerColor = props.root.state.playerColor;
     geIsFirstPlayer = props.root.state.isFirstPlayer;
+    const tileCharacterOneImgLocal = tileCharacterOneImg;
 
     this.state = {
       root_view: props.root,
@@ -77,8 +78,12 @@ export default class GameBoardView extends Component {
       ge_isRoundFinish: false,
       show_scores: false,
       show_pick_character: false,
-      show_move_character: false,
+      show_move_character: false
     };
+  }
+
+  onInitGame() {
+    this.onPickCharacterStart();
   }
 
   onExitGame() {
@@ -86,14 +91,42 @@ export default class GameBoardView extends Component {
     rootClass.setState({ currentView: 1 });
   }
 
-  onInitGame() {
-    this.onPickCharacter();
-  }
-
-  onPickCharacter() {
+  onPickCharacterStart() {
     const currentStatus = this.state;
     currentStatus.show_pick_character = true;
     this.setState(currentStatus);
+  }
+
+  onPickCharacterEnd() {
+    const currentStatus = this.state;
+    currentStatus.show_pick_character = false;
+    this.setState(currentStatus);
+  }
+
+  onMoveCharacterStart() {
+    const currentStatus = this.state;
+    currentStatus.move_pick_character = true;
+    this.setState(currentStatus);
+  }
+
+  onMoveCharacterEnd() {
+    const currentStatus = this.state;
+    currentStatus.move_pick_character = false;
+    this.setState(currentStatus);
+  }
+
+  onShowScoresStart() {
+    // TODO Remove this case in view render method ???
+    // // <ScoresPopUp gameView={this} />
+    const currentState = this.state;
+    currentState.show_scores = true;
+    this.setState(currentState);
+  }
+
+  onShowScoresEnd() {
+    const currentState = this.state;
+    currentState.show_scores = false;
+    this.setState(currentState);
   }
 
   onQuit() {
@@ -107,38 +140,18 @@ export default class GameBoardView extends Component {
     );
   }
 
-  onShowScores() {
-    // TODO Remove this case in view render method ???
-    // // <ScoresPopUp gameView={this} />
+  enableButtons(isEnabled) {
     const currentState = this.state;
-    currentState.show_scores = true;
-    this.setState(currentState);
-  }
-
-  onNotShowScores() {
-    const currentState = this.state;
-    currentState.show_scores = false;
+    currentState.isFirstFloorEnabled = isEnabled;
+    currentState.isSecondFloorEnabled = isEnabled;
+    currentState.isThirdFloorEnabled = isEnabled;
+    currentState.isFourthFloorEnabled = isEnabled;
+    currentState.isFifthFloorEnabled = isEnabled;
+    currentState.isThroneFloorEnabled = isEnabled;
     this.setState(currentState);
   }
 
   render() {
-    if (this.state.show_move_character) {
-      // show_move_character
-      return (
-        <View style={GameBoardStyles.mainStyle}>
-          <Floor floorStyle={GameBoardStyles.throneStyle} imageStyle={GameBoardStyles.shieldImg} totalPos='1' />
-        </View>
-      );
-    }
-    if (this.state.show_pick_character) {
-      // show_pick_character
-      return (
-        <View style={GameBoardStyles.mainStyle}>
-          <Floor floorStyle={GameBoardStyles.throneStyle} imageStyle={GameBoardStyles.shieldImg} totalPos='1' />
-        </View>
-      );
-    }
-    // Board ready
     return (
       <View style={GameBoardStyles.mainStyle}>
         <Floor floorStyle={GameBoardStyles.throneStyle} imageStyle={GameBoardStyles.shieldImg} totalPos='1' />
@@ -155,7 +168,7 @@ export default class GameBoardView extends Component {
           >SELECT</Button>
           <Button
             style={{ color: 'grey', backgroundColor: 'white', borderWidth: 1, borderColor: 'black' }}
-            onPress={() => this.onShowScores()}
+            onPress={() => this.onShowScoresStart()}
           >SCORES</Button>
           <Button
             style={{ color: 'red', backgroundColor: 'white', borderWidth: 1, borderColor: 'black' }}
@@ -166,7 +179,7 @@ export default class GameBoardView extends Component {
         <PopupDialog
           show={this.state.show_scores}
           dialogTitle={<DialogTitle title="SCORES" />}
-          onDismissed={() => this.onNotShowScores()}
+          onDismissed={() => this.onShowScoresEnd()}
         >
           <TableView>
             <Section>
@@ -179,36 +192,110 @@ export default class GameBoardView extends Component {
             </Section>
           </TableView>
         </PopupDialog>
+        {/* Pick a candidate to set on board*/}
+        <PopupDialog
+          show={this.state.show_pick_character}
+          overlayOpacity='0.8'
+          height={400}
+          dialogTitle={<DialogTitle title="PICK A CHARACTER" />}
+          onDismissed={() => this.onPickCharacterEnd()}
+        >
+          <View style={{ flex: 1 }}>
+            <DialogCharacterRow gameView={this} imgA={tileCharacterOneImg} imgB={tileCharacterTwoImg} imgC={tileCharacterThreeImg} mainJustify='space-between' />
+            <DialogCharacterRow gameView={this} imgA={tileCharacterFourImg} imgB={tileCharacterFiveImg} mainJustify='space-around' />
+            <DialogCharacterRow gameView={this} imgA={tileCharacterSixImg} imgB={tileCharacterSevenImg} imgC={tileCharacterEigthImg} mainJustify='space-between' />
+            <DialogCharacterRow gameView={this} imgA={tileCharacterNineImg} imgB={tileCharacterTenImg} mainJustify='space-around' />
+            <DialogCharacterRow gameView={this} imgA={tileCharacterTenOneImg} imgB={tileCharacterTenTwoImg} imgC={tileCharacterTenThreeImg} mainJustify='space-between' />
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+              {this.state.isFirstFloorEnabled ? (
+                <Button style={{ backgroundColor: 'black', color: 'white' }}>Lv 1</Button>
+              ) : (
+                <Text />
+              )}
+              {this.state.isSecondFloorEnabled ? (
+                <Button style={{ backgroundColor: 'black', color: 'white' }}>Lv 2</Button>
+              ) : (
+                <Text />
+              )}
+              {this.state.isThirdFloorEnabled ? (
+                <Button style={{ backgroundColor: 'black', color: 'white' }}>Lv 3</Button>
+              ) : (
+                <Text />
+              )}
+              {this.state.isFourthFloorEnabled ? (
+                <Button style={{ backgroundColor: 'black', color: 'white' }}>Lv 4</Button>
+              ) : (
+                <Text />
+              )}
+              {this.state.isFifthFloorEnabled ? (
+                <Button style={{ backgroundColor: 'black', color: 'white' }}>Lv 5</Button>
+              ) : (
+                <Text />
+              )}
+              {this.state.isThroneFloorEnabled ? (
+                <Button style={{ backgroundColor: 'black', color: 'white' }}>THRONE</Button>
+              ) : (
+                <Text />
+              )}
+            </View>
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-end' }}>
+              <Button style={{ backgroundColor: 'red', color: 'white' }}>RESET</Button>
+            </View>
+          </View>
+        </PopupDialog>
+        {/* TODO Move a candidate to upper floor on board*/}
+        <PopupDialog
+          show={this.state.show_move_character}
+          dialogTitle={<DialogTitle title="SCORES" />}
+          onDismissed={() => this.onMoveCharacterEnd()}
+        >
+          <TableView>
+            <Section>
+              <PlayerCell playerName={playerOne.name} currentScore={playerOne.score_1} />
+            </Section>
+          </TableView>
+        </PopupDialog>
       </View>
     );
   }
 }
 
-const PlayerCell = (props) => (
-  <Cell
-    {...props}
-    cellContentView={
-      <View
-        style={{ alignItems: 'center', flexDirection: 'row', flex: 1, paddingVertical: 10 }}
-      >
-        <Text
-          allowFontScaling
-          numberOfLines={1}
-          style={{ flex: 1, fontSize: 20 }}
-        >
-          {props.playerName}
-        </Text>
+// TODO use rest of them
+const tileCharacterOneImg = require('./../../../res/characters/People_1.png');
+const tileCharacterTwoImg = require('./../../../res/characters/People_2.png');
+const tileCharacterThreeImg = require('./../../../res/characters/People_3.png');
+const tileCharacterFourImg = require('./../../../res/characters/People_4.png');
+const tileCharacterFiveImg = require('./../../../res/characters/People_5.png');
+const tileCharacterSixImg = require('./../../../res/characters/People_6.png');
+const tileCharacterSevenImg = require('./../../../res/characters/People_7.png');
+const tileCharacterEigthImg = require('./../../../res/characters/People_8.png');
+const tileCharacterNineImg = require('./../../../res/characters/People_9.png');
+const tileCharacterTenImg = require('./../../../res/characters/People_10.png');
+const tileCharacterTenOneImg = require('./../../../res/characters/People_11.png');
+const tileCharacterTenTwoImg = require('./../../../res/characters/People_12.png');
+const tileCharacterTenThreeImg = require('./../../../res/characters/People_13.png');
 
-        <Text
-          allowFontScaling
-          numberOfLines={1}
-          style={{ flex: 1, fontSize: 20 }}
-        >
-          Score {props.currentScore}
-        </Text>
-      </View>
-    }
-  />
-);
+
+const characterImgNotSelectedStyle = {
+  width: 80,
+  height: 60,
+  borderTopLeftRadius: 5,
+  borderTopRightRadius: 5,
+  opacity: 0.4,
+  margin: 10,
+  shadowOpacity: 0.9
+};
+
+const characterImgSelectedStyle = {
+  width: 80,
+  height: 60,
+  borderTopLeftRadius: 5,
+  borderTopRightRadius: 5,
+  opacity: 1.0,
+  margin: 10,
+  shadowOpacity: 0.9,
+  shadowColor: 'blue',
+};
+
 // Make Component available to other parts of the app
 //export default GameBoardView;
